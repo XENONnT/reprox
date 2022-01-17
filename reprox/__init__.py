6f43ada194df5a641598dd85185f482ecd6b8da7
Joran Angevaare <jorana@nikhef.nl>
2022-01-17 12:18:57 +0100

    more tests

--- reprox/__init__.py
+++ reprox/__init__.py
@@ -1,7 +1,7 @@
 __version__ = '0.0.0'
 
 from .core import *
-from .determine_data import *
+from .find_runs import *
 from .submit_jobs import *
 from .validate_run import *
 from .process_job import *
