a5b5f00f0b8c9fec5deb205e223e256d25bf5664
Joran Angevaare <jorana@nikhef.nl>
2022-01-17 12:25:57 +0100

    add test

--- tests/test_find_runs.py
+++ tests/test_find_runs.py
@@ -1,8 +1,6 @@
 def test_find_runs():
     from reprox import find_runs
     find_runs.determine_data(
-        targets=targets,
-        exclude_from_invalid_cmt_version=exclude_from_invalid_cmt_version,
-        context_kwargs=context_kwargs,
-        keep_detectors=args.detectors,
+        targets='event_info_double',
+        exclude_from_invalid_cmt_version='global_v6',
     )
